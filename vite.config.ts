c13714023ebbae978bb30ea6ae608b447890c43b
yz <fhlqwe@gmail.com>
2022-10-25 11:49:52 +0800

    feat: remove vite-ignore-style

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
 import { defineConfig } from 'vite'
-import ignoreStylePlugin from 'vite-ignore-style'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ignoreStylePlugin({
-      libraryName: 'antd'
-    })
-  ],
+  plugins: [react()],
   css: {
     preprocessorOptions: {
       less: {
