ac04ffa99712ea5a670b64a6d74733e824683728
yz <fhlqwe@gmail.com>
2022-11-04 16:37:15 +0800

    fix: 移除多余Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { Spin } from 'antd'
-import { Suspense } from 'react'
 import { useRoutes } from 'react-router-dom'
 
 import routerConfig from './routes'
 
 function App() {
   const element = useRoutes(routerConfig)
-  return (
-    <Suspense
-      fallback={
-        <div className='text-center pt-11'>
-          <Spin />
-        </div>
-      }>
-      {element}
-    </Suspense>
-  )
+  return element
 }
 
 export default App
