7a61a551818ee0f847a46a2d4948c5fc3b8905d9
fh <fhlqwe@gmail.com>
2025-07-09 14:56:31 +0800

    feat: integrate NiceModal and React Query with Ant Design in main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,21 @@
 import '@ant-design/v5-patch-for-react-19'
 import './index.css'
 
+import NiceModal from '@ebay/nice-modal-react'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { App, ConfigProvider } from 'antd'
+import zhCn from 'antd/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import dayjsLocal from 'dayjs/locale/zh-cn'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { queryClient } from './comm/query-client'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
+
+dayjs.locale(dayjsLocal)
 // Create a new router instance
 const router = createRouter({
   routeTree,
@@ -26,6 +35,14 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement)
 root.render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ConfigProvider locale={zhCn} theme={{ cssVar: true, hashed: false }}>
+        <App>
+          <NiceModal.Provider>
+            <RouterProvider router={router} />
+          </NiceModal.Provider>
+        </App>
+      </ConfigProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
