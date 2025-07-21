1073c567cc70c0d40f95798c7ea3a1733389ed76
fh <fhlqwe@gmail.com>
2025-07-21 14:52:43 +0800

    chore: update lint-staged config with specific file type rules

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
 export default {
-  '*': ['pnpm run check'],
+  '**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': ['oxlint', 'eslint'],
+  '*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': ['biome format --no-errors-on-unmatched'],
 }
