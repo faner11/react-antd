f2ad1d2a1e8c97ddd7c47633016025f857194f97
fh <fhlqwe@gmail.com>
2025-08-05 14:59:15 +0800

    build(deps): update dependencies and devDependencies

--- release.config.js
+++ release.config.js
@@ -1,5 +1,3 @@
-import packageJson from './package.json' with { type: 'json' }
-
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
@@ -7,23 +5,8 @@ export default {
   branches: ['master'],
   plugins: [
     '@semantic-release/commit-analyzer',
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'conventionalcommits',
-        host: 'https://cn-north-1.console.amazonaws.cn',
-        presetConfig: {
-          commitUrlFormat: `{{host}}/codesuite/codecommit/repositories/${packageJson.name}/commit/{{hash}}`,
-          compareUrlFormat: `{{host}}/codesuite/codecommit/repositories/${packageJson.name}/compare/refs/tags/{{previousTag}}/.../refs/tags/{{currentTag}}`,
-        },
-      },
-    ],
-    [
-      '@semantic-release/changelog',
-      {
-        changelogTitle: `# ${packageJson.name.toUpperCase()} 更新日志`,
-      },
-    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git',
   ],
